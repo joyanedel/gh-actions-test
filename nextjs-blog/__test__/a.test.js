11fc60a88eb66a141a5825877c2c6b25f7866576
Jesús Oyanedel <joyanede@alumnos.inf.utfsm.cl>
2023-07-24 09:38:31 -0400

    Trigger front tests 2

--- nextjs-blog/__test__/a.test.js
+++ nextjs-blog/__test__/a.test.js
@@ -6,16 +6,4 @@ describe('test', () => {
     it('should return 2', () => {
         expect(2).toBe(2)
     })
-
-    it('should return 3', () => {
-        expect(3).toBe(3)
-    })
-
-    it('should return 4', () => {
-        expect(4).toBe(4)
-    })
-
-    it('should return 5', () => {
-        expect(5).toBe(5)
-    })
 })
